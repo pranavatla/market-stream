cc0e44503d0316515dd543c54cbb52ce8096e595
Sai Pranav Atla <pranavatla@gmail.com>
2026-08-11 12:34:20 +0530

    Support GitHub OIDC subject format

--- terraform/modules/cicd/main.tf
+++ terraform/modules/cicd/main.tf
@@ -53,7 +53,10 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:${var.github_repo}:ref:refs/heads/${var.github_branch}",
+            "repo:${replace(var.github_repo, "/", "@*/")}@*:ref:refs/heads/${var.github_branch}",
+          ]
         }
       }
     }]
